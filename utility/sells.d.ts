cbca60f2365ab519bdc860d4d0826a9443aaa9d7
Pritam <pritamjana360@gmail.com>
2024-04-20 23:40:14 +0530

    V-0.2.7
    Upgrade sells component and sells interface

--- utility/sells.d.ts
+++ utility/sells.d.ts
@@ -1,35 +1,44 @@
 export interface SellsDataInterface {
-    salesData: {
-      date: string;
-      product: {
-        title: string;
-        qty: string;
-        price: string;
-        totalPrice: string;
-      }[];
-      totalPrice: number;
+  salesData: {
+    date: string;
+    product: {
+      title: string;
+      qty: string;
+      price: string;
+      totalPrice: string;
     }[];
-    totalLength: number;
-    totalPrice: number,
-    totalLength: number,
-    startDate:string,
-    endDate:string,
-    totalCashPrice:number,
-    totalOnlineAmount:number,
-    totalReturnAmount:number,
-  }
+    totalPrice: number;
+  }[];
+  totalLength: number;
+  totalPrice: number,
+  totalLength: number,
+  startDate: string,
+  endDate: string,
+  totalCashPrice: number,
+  totalOnlineAmount: number,
+  totalReturnAmount: number,
+}
 
-  export interface LineChartDataInterface {
-    sellsData: {
-      date: string;
-      product: {
-        title: string;
-        qty: string;
-        price: string;
-        totalPrice: string;
-      }[];
-      totalPrice: number;
+export interface LineChartDataInterface {
+  sellsData: {
+    date: string;
+    product: {
+      title: string;
+      qty: string;
+      price: string;
+      totalPrice: string;
     }[];
-    totalLength: number;
-  }
+    totalPrice: number;
+  }[];
+  totalLength: number;
+}
 
+export interface SellsProductArrayInterface {
+  salesProduct: {
+    id: string,
+    title: string,
+    qty: string,
+    price: string,
+    totalPrice: string,
+  }[]
+}
