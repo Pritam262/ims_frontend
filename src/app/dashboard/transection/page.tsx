7f35acc9f7f58ce7a0b7c5a03d1cb29bb443a25c
Pritam <pritamjana360@gmail.com>
2024-04-20 23:43:39 +0530

    V-0.2.9
    Add transection page

--- src/app/dashboard/transection/page.tsx
+++ src/app/dashboard/transection/page.tsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from "react";
 import { useAppContext } from "../../../../context/appContext"
 import dateTimeFormat from "../../../../lib/dateTimeFormat";
+import { FaRegEye } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 interface TransectionInterface {
     salesData: {
         id: string,
@@ -12,10 +14,12 @@ interface TransectionInterface {
         date: string,
     }[]
 }
-export default function TransectionPage() {
+export default function TransectionPage({ params, searchParams }: { params: { slug: string }; searchParams: { [key: string]: string | string[] | undefined } }) {
 
     const { serverIp } = useAppContext();
-
+    const [startDate, setStartDate] = useState<string>(`${searchParams?.startdate}`);
+    const [endDate, setEndDate] = useState(`${searchParams?.enddate}`);
+const router = useRouter();
     const [transectionData, setTransectionData] = useState<TransectionInterface>();
 
     const fetchTransectionData = async (startDate: string, endDate: string) => {
@@ -28,10 +32,17 @@ export default function TransectionPage() {
 
 
     useEffect(() => {
-        fetchTransectionData('2024-01-01', '2024-12-31')
-    }, [])
+        fetchTransectionData(startDate, endDate)
+    }, [startDate, endDate])
     return (
         <div className="w-full">
+                            <div className='w-fit flex items-center justify-between my-1'>
+
+<label htmlFor="stdate" className='mr-4'>Start date</label>  <input type="date" name="stdate" onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setStartDate(e.target.value); router.push(`/dashboard/transection?startdate=${e.target.value}&enddate=${endDate}`) }} defaultValue={startDate} className='border border-black text-black' />
+<label htmlFor="endate" className='ml-4'>End date</label>
+<input type="date" name="endate" onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setEndDate(e.target.value); router.push(`/dashboard/transection?startdate=${startDate}&enddate=${e.target.value}`) }} defaultValue={endDate} className='border border-black text-black ml-2' />
+{/* <button type="submit" className="ml-2 px-2 py-1 border border-black" onClick={()=>router.push(`/dashboard/analysis?startdate=${startDate}&enddate=${endDate}`)}>Submit</button> */}
+</div>
             <table className="w-full text-left">
                 <thead className="text-xl bg-slate-400 text-black">
                     <tr>
@@ -41,6 +52,7 @@ export default function TransectionPage() {
                         <th className="p-2">TRANS ID</th>
                         <th className="p-2">MODE</th>
                         <th className="p-2">TOTAL PRICE</th>
+                        <th className="p-2"></th>
                     </tr>
 
                 </thead>
@@ -55,6 +67,7 @@ export default function TransectionPage() {
                             <td className="text-sm px-2 py-1">{data?.transId}</td>
                             <td className="text-sm px-2 py-1">{data?.paymenttype}</td>
                             <td className="text-sm px-2 py-1">{data?.totalprice}</td>
+                            <td className="text-sm px-2 py-1 cursor-pointer"><FaRegEye className="text-yellow-600" onClick={()=>router.push(`/dashboard/transection/${data?.transId}`)}/></td>
                         </tr>
 
                     ))}
