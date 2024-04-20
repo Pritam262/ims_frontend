9f8c8da22425a3da35f2bab65c6dcd5990c84cf5
Pritam <pritamjana360@gmail.com>
2024-04-20 23:46:12 +0530

    V-0.3.0
    Update analysis page

--- src/app/dashboard/analysis/page.tsx
+++ src/app/dashboard/analysis/page.tsx
@@ -5,10 +5,10 @@ import Sells from '../../../../components/Sells'
 import { useState } from 'react';
 import { useAppContext } from '../../../../context/appContext';
 import { useRouter } from 'next/navigation';
-import { SellsDataInterface } from '../../../../utility/sells';
+import { SellsDataInterface, SellsProductArrayInterface } from '../../../../utility/sells';
 
 
-export default function Page({params,searchParams}:{params:{slug:string}; searchParams:{[key:string]:string | string[] | undefined}}) {
+export default function Page({ params, searchParams }: { params: { slug: string }; searchParams: { [key: string]: string | string[] | undefined } }) {
     const { serverIp } = useAppContext();
     const router = useRouter();
 
@@ -24,23 +24,38 @@ export default function Page({params,searchParams}:{params:{slug:string}; search
                     {
                         title: '',
                         qty: '',
-                        price:'',
+                        price: '',
                         totalPrice: '',
                     }
                 ],
                 totalPrice: 0
             }
         ],
-        startDate:'',
-        endDate:'',
+        startDate: '',
+        endDate: '',
         totalPrice: 0,
         totalLength: 0,
-        totalCashPrice:0,
-        totalOnlineAmount:0,
-        totalReturnAmount:0,
+        totalCashPrice: 0,
+        totalOnlineAmount: 0,
+        totalReturnAmount: 0,
     };
 
+    const initialSalesProduct: SellsProductArrayInterface = {
+        salesProduct: [
+            {
+                id: '',
+                title: '',
+                qty: '',
+                price: '',
+                totalPrice: '',
+            }
+        ]
+    }
+
+
     const [salesData, setSalesData] = useState<SellsDataInterface>(initialSalesData);
+
+    const [products, setproducts] = useState<SellsProductArrayInterface>(initialSalesProduct);
 
     //   const fontColor = (theme === "system") ? userTheme() === 'dark' ? "#fff" : "#000" : (theme === 'dark') ? "#fff" : "#000";
     //   const tableFontColor = (theme === "system") ? userTheme() === 'dark' ? "#000" : "#fff" : (theme === 'dark') ? "#000" : "#fff";
@@ -62,14 +77,37 @@ export default function Page({params,searchParams}:{params:{slug:string}; search
                 },
             });
             const data = await response.json()
-            console.log("Range sellsbar data", data)
+            // console.log("Range sellsbar data", data)
             setSalesData(data);
         } catch (error) {
             console.error(error);
         }
     };
+
+    const sellsData = async (startDate: string, endDate: string) => {
+        const stDate = startDate; // Replace with your desired start date
+        const enDate = endDate; // Replace with your desired end date
+
+        try {
+            const response = await fetch(`${serverIp}/api/sells/salesproduct?startDate=${startDate}&endDate=${endDate}`, {
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            const data = await response.json()
+            // console.log("Sells Data", data)
+            setproducts(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
     useEffect(() => {
-        rangeSellData(startDate, endDate)
+        rangeSellData(startDate, endDate);
+        sellsData(startDate, endDate)
     }, [startDate, endDate])
 
     return (
@@ -78,9 +116,9 @@ export default function Page({params,searchParams}:{params:{slug:string}; search
             <div className="w-full h-full bg-slate-200 text-black px-2" >
                 <div className='w-fit flex items-center justify-between my-1'>
 
-                  <label htmlFor="stdate" className='mr-4'>Start date</label>  <input type="date" name="stdate" onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setStartDate(e.target.value)} defaultValue={startDate}  className='border border-black text-black'/>
-                  <label htmlFor="endate" className='ml-4'>End date</label>
-                    <input type="date" name="endate" onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setEndDate(e.target.value)}  defaultValue={endDate} className='border border-black text-black ml-2' />
+                    <label htmlFor="stdate" className='mr-4'>Start date</label>  <input type="date" name="stdate" onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setStartDate(e.target.value); router.push(`/dashboard/analysis?startdate=${e.target.value}&enddate=${endDate}`) }} defaultValue={startDate} className='border border-black text-black' />
+                    <label htmlFor="endate" className='ml-4'>End date</label>
+                    <input type="date" name="endate" onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setEndDate(e.target.value); router.push(`/dashboard/analysis?startdate=${startDate}&enddate=${e.target.value}`) }} defaultValue={endDate} className='border border-black text-black ml-2' />
                     {/* <button type="submit" className="ml-2 px-2 py-1 border border-black" onClick={()=>router.push(`/dashboard/analysis?startdate=${startDate}&enddate=${endDate}`)}>Submit</button> */}
                 </div>
                 {/* Analysis card component */}
@@ -108,7 +146,7 @@ export default function Page({params,searchParams}:{params:{slug:string}; search
                 {/* Analysis card component end */}
 
                 <div className="h-fit border border-black">
-                    <Sells  salesData={salesData.salesData} totalPrice={salesData.totalPrice} startDate={salesData?.startDate} endDate={salesData?.endDate} totalLength={salesData.totalLength} totalCashPrice={salesData?.totalCashPrice} totalOnlineAmount={salesData?.totalOnlineAmount} totalReturnAmount={salesData?.totalReturnAmount}/>
+                    <Sells salesData={salesData.salesData} totalPrice={salesData.totalPrice} startDate={salesData?.startDate} endDate={salesData?.endDate} totalLength={salesData.totalLength} totalCashPrice={salesData?.totalCashPrice} totalOnlineAmount={salesData?.totalOnlineAmount} totalReturnAmount={salesData?.totalReturnAmount} />
                 </div>
 
                 {/* Dashboard  static graph table start */}
@@ -128,8 +166,16 @@ export default function Page({params,searchParams}:{params:{slug:string}; search
 
                         <table className="w-full text-left">
                             <thead className="text-xl bg-slate-400 text-black">
+                                {/* <tr>
+                                    <th className="p-2">#</th>
+                                    <th className="p-2">PRODUCT NAME</th>
+                                    <th className="p-2">QTY</th>
+                                    <th className="p-2">PRICE</th>
+                                    <th className="p-2">TOTAL</th>
+                                </tr> */}
                                 <tr>
                                     <th className="p-2">#</th>
+                                    <th className="p-2">ID</th>
                                     <th className="p-2">PRODUCT NAME</th>
                                     <th className="p-2">QTY</th>
                                     <th className="p-2">PRICE</th>
@@ -138,10 +184,24 @@ export default function Page({params,searchParams}:{params:{slug:string}; search
 
                             </thead>
                             <tbody>
-                                {salesData.salesData && salesData.salesData.map((data, index) => (
+
+                                {products.salesProduct && products.salesProduct.map((data, index) => (
+
+
+                                    <tr key={index} className="border-black" style={index % 2 ? { backgroundColor: "#c6c6c6", color: "#000" } : {}}>
+                                        <td className="text-sm px-2">{index + 1}</td>
+                                        <td className="text-sm px-2">{data?.id}</td>
+                                        <td className="text-sm px-2">{data?.title}</td>
+                                        <td className="text-sm px-2">{data?.qty}</td>
+                                        <td className="text-sm px-2">{data?.price}</td>
+                                        <td className="text-sm px-2">{data?.totalPrice}</td>
+                                    </tr>
+
+                                ))}
+                                {/* {salesData.salesData && salesData.salesData.map((data, index) => (
                                     data.product.map((item, index) => (
 
-                                        <tr key={index} className="border-black" style={index % 2 ? { backgroundColor: "#c6c6c6", color:"#000"} : {}}>
+                                        <tr key={index} className="border-black" style={index % 2 ? { backgroundColor: "#c6c6c6", color: "#000" } : {}}>
                                             <td className="text-sm px-2">{index + 1}</td>
                                             <td className="text-sm px-2">{item.title}</td>
                                             <td className="text-sm px-2">{item.qty}</td>
@@ -149,13 +209,13 @@ export default function Page({params,searchParams}:{params:{slug:string}; search
                                             <td className="text-sm px-2">{item.totalPrice}</td>
                                         </tr>
                                     ))
-                                ))}
+                                ))} */}
                             </tbody>
                         </table>
                     </div>
 
-                       {/* Static graph */}
-                       <div className="w-2/4 h-full border border-black">
+                    {/* Static graph */}
+                    <div className="w-2/4 h-full border border-black">
                         <div className="flex mt-2 mx-1">
                             <p>Static Graph</p>
                             <select className="ml-4 text-black bg-transparent" id='catoption' style={{ color: "#000" }}>
